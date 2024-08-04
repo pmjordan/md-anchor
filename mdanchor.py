34d706d3822a08dcff5d653879d370fa106c9674
Paul Jordan <paul.m.jordan@outlook.com>
2024-08-04 22:02:34 +0100

    remove brackets

--- mdanchor.py
+++ mdanchor.py
@@ -1,9 +1,23 @@
 import re
 
-def add_anchors_to_markdown(input_file_path, output_file_path, text_to_add):
+
+# ways to do anchor
+
+
+#\[See “nil”, below\](#nil)
+#  #### 9.1.1.6 nil 
+#but the slug includes 9116
+
+# so try 
+# ### This is the Heading <a name="this-is-the-heading"></a>
+
+# TODO add real anchors
+# TODO don't add anchors to lines which already have them
+
+def add_anchors_to_markdown(input_file_path, output_file_path):
     # Define the pattern you want to match
     pattern = re.compile(r'^([#])+\s([0-9]+)')
-
+    text_to_add =''
     # Open the input file for reading and the output file for writing
     with open(input_file_path, 'r') as infile, open(output_file_path, 'w') as outfile:
         # Process each line in the input file
@@ -16,8 +30,8 @@ def add_anchors_to_markdown(input_file_path, output_file_path, text_to_add):
                 
                 # Ignore the first token and concatenate the rest with an underscore
                 if len(tokens) > 1:
-                    line=tokens[0]+' ('+' '.join(tokens[1:])+')'
-                    concatenated_tokens = '_'.join(tokens[2:]).lower()
+                    line=tokens[0]+' '.join(tokens[1:])
+                    concatenated_tokens = '-'.join(tokens[2:]).lower()
                     
                     #print(concatenated_tokens)
 
@@ -29,4 +43,4 @@ def add_anchors_to_markdown(input_file_path, output_file_path, text_to_add):
     print(f'Processed lines written to {output_file_path}')
 
 # Example usage:
-add_anchors_to_markdown('test.md', 'output.md', '')
+add_anchors_to_markdown('test.md', 'output.md', 'text')
