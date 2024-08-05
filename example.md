38aa936889db13b7ab6c9e3809a7116d6e94c9f3
Paul Jordan <paul.m.jordan@outlook.com>
2024-08-05 07:06:05 +0100

    first release

---
+++ example.md
@@ -0,0 +1,10 @@
+3.  TOSCA bytes values *cannot* be the null value but *can* be empty
+    arrays (a bytes value with length zero). \[See “nil”, below\](#nil)
+[go to heading](#this-is-the-heading)
+
+
+
+
+
+### This is the Heading <a name="this-is-the-heading"></a>
+#### 9.1.1.6 nil 
--- mdanchor.py
+++ mdanchor.py
@@ -17,7 +17,7 @@ import re
 def add_anchors_to_markdown(input_file_path, output_file_path):
     # Define the pattern you want to match
     pattern = re.compile(r'^([#])+\s([0-9]+)')
-    text_to_add =''
+    text_to_add ='<a name="heading"></a>'
     # Open the input file for reading and the output file for writing
     with open(input_file_path, 'r') as infile, open(output_file_path, 'w') as outfile:
         # Process each line in the input file
@@ -27,20 +27,19 @@ def add_anchors_to_markdown(input_file_path, output_file_path):
 
                 # Tokenize the line
                 tokens = line.split()
-                
+
                 # Ignore the first token and concatenate the rest with an underscore
                 if len(tokens) > 1:
-                    line=tokens[0]+' '.join(tokens[1:])
                     concatenated_tokens = '-'.join(tokens[2:]).lower()
-                    
-                    #print(concatenated_tokens)
-
-                line = line.strip() + ' ' + text_to_add 
-                line = line.strip()+ '\n'
+                    text_to_add = '<a name="'+concatenated_tokens+'"></a>' 
+                
+                #remove the newline, add teh anchor and add a new line
+                line = line.strip() + ' ' + text_to_add + '\n'
+                
             # Write the (possibly modified) line to the output file
             outfile.write(line)
 
     print(f'Processed lines written to {output_file_path}')
 
 # Example usage:
-add_anchors_to_markdown('test.md', 'output.md', 'text')
+add_anchors_to_markdown('test.md', 'output.md')
